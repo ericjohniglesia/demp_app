c2b76c38edacfe35c34f731581f9332f53a09d85
U-ERIC-PC\Eric John Iglesia <Eric John Iglesia@eric-pc.(none)>
2014-02-19 13:02:27 +0800

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Irene1::Application.routes.draw do
+  resources :users
+
   get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
